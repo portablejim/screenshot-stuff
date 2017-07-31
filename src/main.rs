c1248bd0722bc5fb545ea3bea21c0aa2b644d88f
portablejim <dcseee-github@yahoo.com.au>
2017-07-31 13:35:33 +1000

    Get capturing only keyframes

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,43 @@
 extern crate image;
 extern crate scrap;
 extern crate time;
-extern crate twox_hash;
 extern crate dxgcap;
 
 use image::{ImageBuffer, Rgba};
-use scrap::{Capturer, Display};
-use std::io::ErrorKind::WouldBlock;
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender, Receiver};
-use twox_hash::XxHash;
-use std::hash::BuildHasherDefault;
-use std::hash::Hasher;
 use dxgcap::DXGIManager;
+use dxgcap::BGRA8;
+use dxgcap::CaptureError;
 
+#[derive(Clone)]
 struct FrameInfo {
     time: u64,
     w: usize,
     h: usize,
-    frame: Vec<u8>,
+    frame: Vec<BGRA8>,
 }
 
 fn main() {
     let one_second = Duration::new(1, 0);
-    let one_frame = one_second / 20;
+    let one_frame = one_second / 5;
 
-    let mut displays = Display::all().expect("Couldn't find displays.");
-    let display = displays.remove(0);
-    
-    let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
-    let (w, h) = (capturer.width(), capturer.height());
+    let mut manager = DXGIManager::new(200).expect("Unable to make manager.");
+    manager.set_capture_source_index(1);
+    //manager.acquire_output_duplication();
 
-    let pixels = w * h * 4;
+    //let pixels = w * h * 4;
     //let (w2, h2) = (&w, *h);
 
     // Setup threads
     let (tx_all, rx_all): (Sender<FrameInfo>, Receiver<FrameInfo>) = mpsc::channel();
-    let (tx_filtered, rx_filtered): (Sender<FrameInfo>, Receiver<FrameInfo>) = mpsc::channel();
     let handle = thread::spawn(move || {
         let mut i = 0;
 
-        let mut last_saved: Vec<u8> = vec![0; pixels];
+        let mut last_saved: Option<Vec<BGRA8>> = None;
 
         for frameinfo in rx_all {
 
@@ -52,43 +46,39 @@ fn main() {
             let h = frameinfo.h;
             let buffer = frameinfo.frame;
 
-            if last_saved != buffer {
-                // PistonDevelopers/image doesn't support ARGB images yet.
-                // But they will soon!
-                let mut bitflipped = Vec::with_capacity(w * h * 4);
-                for pixel in buffer.chunks(4) {
-                    let (b, g, r, a) = (pixel[0], pixel[1], pixel[2], pixel[3]);
-                    bitflipped.extend_from_slice(&[r, g, b, a]);
-                }
+            last_saved = match last_saved {
+                None => Some(buffer),
+                Some(last_saved) => {
+                    if last_saved == buffer {
+                        println!("Ignored frame");
+                        Some(last_saved)
+                    }
+                    else {
+                        let mut bitflipped = Vec::with_capacity(w * h * 4);
+                        for pixel in &buffer {
+                            //let (b, g, r, a) = (pixel[0], pixel[1], pixel[2], pixel[3]);
+                            //bitflipped.extend_from_slice(&[r, g, b, a]);
+                            bitflipped.extend_from_slice(&[pixel.r, pixel.g, pixel.b, pixel.a])
+                        }
 
-                let mut hash: XxHash = XxHash::default();
-                hash.write(&buffer);
-                let hash_value = hash.finish();
+                        let pathname = format!("screenshot{}.png", i);
+                        let path = Path::new(&pathname);
 
-                let pathname = format!("screenshot{:X}.png", hash_value);
-                let path = Path::new(&pathname);
+                        let image: ImageBuffer<Rgba<u8>, _> =
+                            ImageBuffer::from_raw(w as u32, h as u32, bitflipped)
+                                .expect("Couldn't convert frame into image buffer.");
 
-                if path.exists() {
-                    println!("Image already saved to `{}` @ {}.", pathname, frametime);
-                }
-                else {
-                    let image: ImageBuffer<Rgba<u8>, _> =
-                        ImageBuffer::from_raw(w as u32, h as u32, bitflipped)
-                            .expect("Couldn't convert frame into image buffer.");
+                        image.save(&path).expect(&format!(
+                            "Couldn't save image to `screenshot{}.png`.",
+                            i
+                        ));
+                        println!("Image saved to `{}` @ {}.", pathname, frametime);
+                        i += 1;
 
-                    image.save(&path).expect(&format!(
-                        "Couldn't save image to `screenshot{}.png`.",
-                        i
-                    ));
-                    println!("Image saved to `{}` @ {}.", pathname, frametime);
-                    i += 1;
-                }
-                last_saved = buffer;
-
-            }
-            else {
-                println!("Ignored frame");
-            }
+                        Some(buffer)
+                    }
+                },
+            };
         }
     });
 
@@ -98,31 +88,52 @@ fn main() {
 
         let base_epoch = time::precise_time_ns();
 
+        let mut frameinfo_last: Option<FrameInfo> = None;
         for _ in 0..200 {
             loop {
-                let buffer = match capturer.frame() {
-                    Ok(buffer) => buffer,
+                let (buffer,w,h) = match manager.capture_frame() {
+                    Ok((buffer, (w,h))) => (buffer,w,h),
+                    Err(CaptureError::Timeout) => {
+                        match frameinfo_last.clone() {
+                            None => continue,
+                            Some(frameinfo) => {
+                                tx1.send(frameinfo).expect("Error sending raw image data.");
+                                frameinfo_last = None;
+                                break;
+                            }
+                        }
+                    },
                     Err(error) => {
-                        if error.kind() == WouldBlock {
+                        /*if error.kind() == WouldBlock {
                             // Keep spinning.
                             thread::sleep(one_frame);
                             continue;
                         } else {
                             panic!("Capture error: {}", error);
-                        }
+                        }*/
+                        println!("Error: {:?} -> Sleeping for {:?}", error, one_frame);
+                        thread::sleep(one_frame);
+                        continue;
                     }
                 };
 
+                frameinfo_last = Some(FrameInfo {
+                    time: (time::precise_time_ns() - base_epoch) / 1_000_000,
+                    w: w,
+                    h: h,
+                    frame: buffer,
+                });
+
                 //println!("Captured! Saving...");
 
-                tx1.send(FrameInfo {
+                /*tx1.send(FrameInfo {
                     time: (time::precise_time_ns() - base_epoch) / 1_000_000,
                     w: w,
                     h: h,
                     frame: (*buffer).iter().cloned().collect(),
-                }).expect("Error sending raw image data.");
+                }).expect("Error sending raw image data.");*/
 
-                break;
+                continue;
             }
 
         }

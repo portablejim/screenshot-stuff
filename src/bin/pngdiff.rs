7f8bd01619d553761535f46477ce5e6123e71dad
portablejim <dcseee-github@yahoo.com.au>
2017-08-04 11:29:55 +1000

    Get diff working without dedupe

--- src/bin/pngdiff.rs
+++ src/bin/pngdiff.rs
@@ -3,6 +3,7 @@ extern crate serde_json;
 extern crate oxipng;
 //extern crate imagequant;
 extern crate png;
+extern crate twox_hash;
 
 use std::env;
 use std::path::Path;
@@ -11,13 +12,16 @@ use std::fs;
 use std::char;
 use std::io::Read;
 use std::collections::BTreeSet;
-use std::collections::HashMap;
 use std::ascii::AsciiExt;
 use image::GenericImage;
 use image::Rgba;
 use image::Pixel;
 use image::DynamicImage;
 use std::io::Write;
+use std::hash::BuildHasherDefault;
+use std::collections::HashMap;
+use twox_hash::XxHash;
+use std::hash::Hasher;
 
 extern crate imagefmt;
 use imagefmt::{ColFmt, ColType};
@@ -27,6 +31,7 @@ fn main() {
 
     if args.len() == 2 {
         let image_hashes: HashMap<usize, String> = HashMap::new();
+        let mut image_hashes2: HashMap<u64, Vec<usize>> = HashMap::new();
 
         let mut timings_file: String = String::new();
         File::open(args.get(1).expect("Error getting path")).expect("No such file").read_to_string(&mut timings_file);
@@ -41,89 +46,110 @@ fn main() {
             return;
         }
 
-        let mut previous: Option<&DynamicImage> = None;
-        let mut entry_num = 0;
-        for entry_num in 2..timings.len() {
+        let mut previous: Option<DynamicImage> = None;
+        let mut entry_num: usize = 0;
+        println!("{}", timings.len());
+        for entry_num in 0..timings.len() {
             let entry = timings.get(entry_num).expect("Weird length for timings");
             if entry.len() >= 2 {
                 let entry_image = timings_dir.join(entry.get(1).expect("No path supplied"));
-                match previous {
+                previous = match previous {
                     None => {
                         // First entry
                         match image::open(timings_dir.join(entry_image)) {
-                            Ok(image) => {
-                                let optimised_image = optimise(image, 75);
-                                let px =  (optimised_image.to_rgb());//.into_raw();
+                            Ok(image_data) => {
+                                // Generate hash
+                                let mut hasher = XxHash::default();
+                                for pixel in image_data.raw_pixels() {
+                                    hasher.write_u8(pixel);
+                                }
+                                image_hashes2.insert(hasher.finish(), vec![entry_num]);
 
+                                // Setup oxipng
+                                let out_name = format!("slide{:03}.png", entry_num+1);
                                 let mut oxioptions = oxipng::Options::from_preset(4);
                                 oxioptions.interlace = Some(1);
                                 oxioptions.verbosity = Some(1);
-                                let out_name = format!("slide{:03}.png", entry_num);
+                                oxioptions.out_file = images_path.join(&out_name);
                                 let out_relpath = match images_path.file_name().and_then(|n|n.to_str()) {
                                     Some(images_path_name) => format!("{}/{}", images_path_name, out_name),
                                     None => String::new()
                                 };
 
 
+                                // Save png with oxipng
                                 let mut image_vec: Vec<u8> = Vec::new();
-                                let image_writer = png::Writer::new(image_hashes);
+                                image_data.save(&mut image_vec, image::PNG);
 
-                                oxioptions.out_file = images_path.join(&out_name);
                                 println!("out: {}", oxioptions.out_file.to_str().unwrap_or("(none)"));
-                                let oxi_output = oxipng::optimize_from_memory(&px, &oxioptions).expect("Error creating compressed image");
+                                let oxi_output = oxipng::optimize_from_memory(&image_vec, &oxioptions).expect("Error creating compressed image_data");
                                 File::create(images_path.join(&out_name)).expect("Error writing").write(&oxi_output);
-                                //}
+
+                                Some(image_data)
                             }
                             Err(e) => {
                                 eprintln!("Error when reading {:?}", e);
+                                None
                             }
                         }
                     }
-                    Some(previousEntry) => {
+                    Some(ref previous_entry) => {
+                        match image::open(timings_dir.join(entry_image)) {
+                            Ok(image_data) => {
+                                // Generate hash
+                                let mut hasher = XxHash::default();
+                                for pixel in image_data.raw_pixels() {
+                                    hasher.write_u8(pixel);
+                                }
+                                image_hashes2.insert(hasher.finish(), vec![entry_num]);
 
+                                let (image_diff, diff_percent) = diff2(previous_entry, &image_data);
+
+                                // Setup oxipng
+                                let out_name = format!("slide{:03}.png", entry_num + 1);
+                                let mut oxioptions = oxipng::Options::from_preset(4);
+                                //oxioptions.interlace = Some(1);
+                                oxioptions.verbosity = Some(1);
+                                oxioptions.out_file = images_path.join(&out_name);
+                                let out_relpath = match images_path.file_name().and_then(|n| n.to_str()) {
+                                    Some(images_path_name) => format!("{}/{}", images_path_name, out_name),
+                                    None => String::new()
+                                };
+
+                                // Save png with oxipng
+                                let mut image_vec: Vec<u8> = Vec::new();
+                                image_diff.save(&mut image_vec, image::PNG);
+
+                                println!("out: {}", oxioptions.out_file.to_str().unwrap_or("(none)"));
+                                let oxi_output = oxipng::optimize_from_memory(&image_vec, &oxioptions).expect("Error creating compressed image_data");
+                                File::create(images_path.join(&out_name)).expect("Error writing").write(&oxi_output);
+
+                                Some(image_data)
+                            }
+                            Err(e) => {
+                                eprintln!("Error when reading {:?}", e);
+                                None
+                            }
+                        }
                     }
                 }
             }
         }
-        /*
-        let dira = fs::read_dir(Path::new(args.get(1).expect("Error getting directory"))).expect("Error getting directory contents");
-        let png_list: Vec<_> = dira.filter_map(|d|d.ok().and_then(|e| Some(e.file_name()))).filter(|entry| {
-            entry.to_str().unwrap_or("").to_ascii_lowercase().ends_with(".png")
-        }).collect();
-        println!("{:?}", png_list);*/
     }
     return;
 }
 
-fn optimise(image: DynamicImage, min_quality: u8) -> DynamicImage {
-    /*let mut liq = imagequant::new();
-    liq.set_quality(min_quality, 100);
-    liq.set_speed(1);*/
 
-    //let ref mut liq_image = liq.new_image(image, image.width(), image.height())
-    return image;
-}
+fn diff2(imga: &DynamicImage, imgb: &DynamicImage) -> (DynamicImage, u64) {
 
-
-fn diff2(patha: String, pathb: String, pathc: String) {
-
-        let imga = image::open(&Path::new(&patha));
-        let imgb = image::open(&Path::new(&pathb));
-
-        match (imga, imgb) {
-            (Err(_), Err(_)) => return,
-            (Ok(_), Err(_)) => return,
-            (Err(_), Ok(_)) => return,
-            (Ok(imga), Ok(imgb)) => {
                 let (w, h) = imga.dimensions();
 
                 let mut imgc = image::DynamicImage::new_rgba8(w, h);
 
                 let mut pixels_same: u64 = 0;
                 let mut pixels_notsame: u64 = 0;
-                let mut colours = BTreeSet::new();
-                for x in 0..w {
-                    for y in 0..h {
+                for y in 0..h {
+                    for x in 0..w {
                         let pixel_a = imga.get_pixel(x, y);
                         let pixel_b = imgb.get_pixel(x, y);
 
@@ -132,45 +158,11 @@ fn diff2(patha: String, pathb: String, pathc: String) {
                             pixels_same += 1;
                         } else {
                             imgc.put_pixel(x, y, pixel_b);
-                            let colour_value = ((pixel_b.data[0] as u32) << 16) + ((pixel_b.data[1] as u32) << 8) + pixel_b[2] as u32;
                             pixels_notsame += 1;
-                            if !colours.contains(&colour_value) && colours.len() < 300 {
-                                colours.insert(colour_value);
-                            }
                         }
                     }
                 }
                 let pixels_same_percent = (pixels_same * 100) / (pixels_notsame + pixels_same);
-                let num_colours = match colours.len() {
-                    300 => format!("300+"),
-                    other => format!("{}", other)
-                };
-                println!("Transparent: {}, Colours: {}", pixels_same_percent, num_colours);
 
-                if pixels_notsame == 0 {
-                    // No transparency no alpha channel is just fluff.
-                    //imgc = imgc.to_rgb()
-                    let _ = imagefmt::write(
-                        pathc,
-                        w as usize,
-                        h as usize,
-                        ColFmt::RGB,
-                        &imgc.raw_pixels(),
-                        ColType::Color,
-                    ).unwrap();
-                }
-                else {
-                    let _ = imagefmt::write(
-                        pathc,
-                        w as usize,
-                        h as usize,
-                        ColFmt::RGBA,
-                        &imgc.raw_pixels(),
-                        ColType::ColorAlpha,
-                    ).unwrap();
-                }
-
-            }
-
-        };
+    (imgc, pixels_same_percent)
 }
